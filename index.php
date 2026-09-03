6b90cba225f490b3f1481903262630b884a99857
Marten Erlenhein <marten.erlenhein@gmail.com>
2026-09-03 09:49:45 +0300

    Add math examples

--- index.php
+++ index.php
@@ -1,14 +1,28 @@
 <?php
-$test = 10; // comment
+$test = 10;
+$test = 10 + 10;
+$test = 10 - 10;
+$test = 10 * 10;
+$test = 10 / 10;
+$test = $test + 10;
+$test = 2 ** 3;
+$test = 5 % 2;
+$test += 3;
+$test -= 3;
+$test *= 3;
+$test /= 3;
+$test %= 3;
+$test **= 3;
+$test %= 3;
+$test++;
+$test--;
+--$test;
+++$test;
 
-// asdas
-// ada
-// adsas
 
-/** 
- * asd
- * asd
- * asd
- * asd
- */
+$a = 1;
+$b = ++$a; 
+var_dump($b, $a);
 
+$test = sin(pi());
+var_dump($test);
