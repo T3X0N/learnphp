b9fa645b970fe38df2a8cb5a3302e5c75e6ea610
Marten Erlenhein <marten.erlenhein@gmail.com>
2026-09-08 09:23:24 +0300

    Add array examples

--- index.php
+++ index.php
@@ -1,13 +1,22 @@
 <?php
-$test = 10 > 5;
-$test = 10 < 5;
-$test = 10 == 10;
-$test = 10 <= 10;
-$test = 10 >= 10;
-$test = 10 == '10';
-$test = 10 === '10';
-$test = 10 != '10';
-$test = 10 !== '10';
-$test = 0 == false;
-// https://www.php.net/manual/en/types.comparisons.php
+$test = array(1, 2, 3);
+$test = [1, 2, 3];
+$test = [1, 'asdas', true, [1, 2, 3]];
+var_dump($test[3][1]);
+$test = [
+    1,
+    'name' => 'Kaspar',
+    'age' => 33,
+    3,
+    'aadsas',
+    true,
+    100 => 'lol',
+    'troll',
+];
+var_dump($test['name']);
+array_push($test, 'added', 'zxvxxzcv', 3);
+$test[] = 'new value';
+$test['name'] = 'Martin';
+$test[1] = 99999;
+unset($test[3]);
 var_dump($test);
