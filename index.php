65a68425084e87d84ce49278d11b6b1757261845
Marten Erlenhein <marten.erlenhein@gmail.com>
2026-09-10 09:33:15 +0300

    add class exm

--- index.php
+++ index.php
@@ -1,18 +1,33 @@
 <?php
 
-$test = function (){
-  var_dump('test');
-};
+class box {
+  public $width;
+  public $height;
+  public $depth;
+  public $isopen = false;
+  public $hasbeemopened = false;
 
-var_dump($test);
-$test();
-call_user_func($test);
+  public function open() {
+    if ($this->isopen) {
+      echo "The box is already open.";
+    } else {
+      $this->isopen = true;
+      $this->hasbeemopened = true;
+      echo "The box is now open.";
+    }
+  }
+}
 
-$numbers = [1, 2, 3, 4, 5];
-$squares = array_map(function ($n) {
-  return $n * $n;
-}, $numbers);
-var_dump($squares);
+$box1 = new box();
+$box1->width = 10;
+$box1->height = 5;
+$box1->depth = 8;
+$box1->open();
+var_dump($box1);
 
-$squares2 = array_map(fn($n) => $n * $n, $numbers);
-var_dump($squares2);
+$box2 = new box();
+$box2->width = 12;
+$box2->height = 6;
+$box2->depth = 10;
+$box2->open();
+var_dump($box2);
