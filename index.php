09d6ff7f2109d426c638deeeca89869dfc6175d6
Marten Erlenhein <marten.erlenhein@gmail.com>
2026-09-10 09:11:47 +0300

    add anonyymnsed funki exm

--- index.php
+++ index.php
@@ -1,30 +1,18 @@
 <?php
 
-function hello(): void {
-    var_dump("Hello, World!");
-}
-hello();
+$test = function (){
+  var_dump('test');
+};
 
-function helloName(string $name, int $age): void {
-    var_dump("Hello, $name! You are $age years old.");
-}
-helloName("Alice", 30);
+var_dump($test);
+$test();
+call_user_func($test);
 
-function square(int $number): int {
-    if ($number < 0) {
-        $number = -$number;
-    }
+$numbers = [1, 2, 3, 4, 5];
+$squares = array_map(function ($n) {
+  return $n * $n;
+}, $numbers);
+var_dump($squares);
 
-    return $number * $number;
-}
-$answer = square(5);
-var_dump($answer);
-
-function recursion($i) {
-    if ($i < 20) {
-        var_dump($i);
-        recursion($i + 1);
-    }
-    }
-
-    recursion(0);
+$squares2 = array_map(fn($n) => $n * $n, $numbers);
+var_dump($squares2);
