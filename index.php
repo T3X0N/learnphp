1f36d6057019b1e74184ca9028e29dad9fc80e09
Marten Erlenhein <marten.erlenhein@gmail.com>
2026-09-15 09:40:38 +0300

    Add trait example

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 class Box {
-    
+    use HasSmell;
     public $isOpen = false;
     public $hasBeenOpened = false;
 
@@ -19,12 +19,28 @@ class Box {
 }
 
 class MetalBox extends Box {
+    use HasColor, HasSmell;
     public $weightPerUnit;
     
     public function mass(){
         return $this->weightPerUnit * $this->volume();
     }
 }
+
+trait HasColor {
+    public $color;
+    public function showColor(){
+        return $this->color;
+    }
+}
+
+trait HasSmell {
+    public $smell;
+    public function sniff(){
+        return $this->smell;
+    }
+}
+
 
 $metal1 = new MetalBox(1,2,3);
 $metal1->weightPerUnit = 1;
