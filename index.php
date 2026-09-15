f6ef89fd9ba276b31c32ee0371c2e00444ae9451
Marten Erlenhein <marten.erlenhein@gmail.com>
2026-09-15 09:53:45 +0300

    Add visibility example

--- index.php
+++ index.php
@@ -2,11 +2,21 @@
 
 class Box {
     use HasSmell;
-    public $isOpen = false;
-    public $hasBeenOpened = false;
+    public bool $isOpen = false;
+    private bool $hasBeenOpened = false;
 
-    public function __construct(public $width, public $height, public $length) {
+    public function __construct(private int $width, private $height, private $length) {
       
+    }
+
+    public function setWidth(int $width) {
+        if($width<0){
+            $this->width = 0;
+        }
+        $this->width = $width;
+    }
+    public function getWidth() {
+        return $this->width;
     }
 
     public function open() {
@@ -16,6 +26,9 @@ class Box {
     public function volume(){
         return $this->width * $this->height * $this->length;
     }
+    public function test1() {
+        var_dump($this->hasBeenOpened);
+    }
 }
 
 class MetalBox extends Box {
@@ -24,6 +37,10 @@ class MetalBox extends Box {
     
     public function mass(){
         return $this->weightPerUnit * $this->volume();
+    }
+
+    public function test2() {
+        var_dump($this->hasBeenOpened);
     }
 }
 
@@ -45,3 +62,8 @@ trait HasSmell {
 $metal1 = new MetalBox(1,2,3);
 $metal1->weightPerUnit = 1;
 var_dump($metal1->mass(), $metal1);
+$metal1->isOpen = 'asdasd';
+var_dump($metal1->isOpen);
+$metal1->test1();
+$metal1->test2();
+var_dump($metal1->hasBeenOpened);
