2610b7bc96932b6ad28d4263e9e261feede3ea33
Marten Erlenhein <marten.erlenhein@gmail.com>
2026-09-18 09:30:37 +0300

    Add php tags in html example

--- index.php
+++ index.php
@@ -1,45 +1,24 @@
 <?php
-// library
+$name = 'Kaspar';
+?>
 
-class Task {
-    public function job(Logger $logger) {
-        for($i = 0; $i<10; $i++){
-            // do some shit
-            $logger->log("Job $i was done!");
-        }
-    }
-}
-
-class ConsoleLogger implements Logger {
-    public function log($message) {
-        echo "$message\n";
-    }
-}
-
-class NothingLogger implements Logger {
-    public function log($message) {
-       
-    }
-}
-
-interface Logger {
-    public function log($message);
-}
-
-interface Writer {
-
-}
-
-// user code
-
-class FileLogger implements Logger {
-    public function log($message) {
-        $file = fopen('log.txt', 'a');
-        fwrite($file, "$message\n");
-        fclose($file);
-    }
-}
-
-$logger = new FileLogger();
-$task = new Task();
-$task->job($logger);
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello <?=$name?>!</h1>
+    <ul>
+        <?php for($i=0;$i<10;$i++): ?>
+            <?php if($i%2 === 0): ?>
+                <li style="color:green"><?= $i ?></li>
+            <?php else: ?>
+                 <li style="color:red"><?= $i ?></li>
+            <?php endif ?>
+        <?php endfor ?>
+    </ul>
+</body>
+</html>
